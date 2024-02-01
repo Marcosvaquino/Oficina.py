081ad57efeead16f5e8efd2a5770a8ac4ef1e942
Marcosvaquino <marcosvinicius_a@hotmail.com>
2024-02-01 09:17:15 -0300

    criação da tabela das oficinas

--- Model/oficina.py
+++ Model/oficina.py
@@ -3,5 +3,29 @@ from datetime import datetime
 
 @dataclass
 class Oficina:
+    # Informações Básicas da Empresa
     id:int
     nome:str
+    CNPJ: str
+    razao_social: str
+    nome_fantasia: str
+
+    # Informações de Contato:
+    logradouro: str
+    numero: str
+    bairro: str
+    cidade: str
+    estado: str
+    CEP: str
+    telefone: str
+    email: str
+
+    # Responsáveis e Contato Comercial
+    nome_responsavel: str
+    cargo_responsavel: str
+    telefone_comercial: str
+    email_comercial: str
+
+    # Informações Financeiras:
+    dados_bancarios: str
+    
